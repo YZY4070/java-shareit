86f487d6ac2b35ee5f68a5dc2dac2a471f901609
YZY4070 <muteallchat30@yandex.ru>
2025-03-27 07:36:00 +0300

    checstyle

--- gateway/src/test/java/ru/practicum/shareit/item/CommentRequestTests.java
+++ gateway/src/test/java/ru/practicum/shareit/item/CommentRequestTests.java
@@ -1,17 +1,11 @@
 package ru.practicum.shareit.item;
 
-import jakarta.validation.ConstraintViolation;
-import jakarta.validation.Validation;
-import jakarta.validation.Validator;
-import jakarta.validation.ValidatorFactory;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.json.JsonTest;
 import org.springframework.boot.test.json.JacksonTester;
 import org.springframework.boot.test.json.JsonContent;
 import ru.practicum.shareit.item.dto.CommentRequestDto;
-
-import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
